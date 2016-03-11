74f376b8871d58ed382c1c832f6ff5234b105606
Aaron Meurer <asmeurer@gmail.com>
2016-03-11 15:25:21 -0500

    Add some more

--- pi_sympy.py
+++ pi_sympy.py
@@ -48,17 +48,109 @@ def pi_012():
 # def pi_014():
 #     return solve(besselj(S(1)/2, x), x)[0]
 
-# def pi_015():
-#     return simplify(3*sqrt(3)/2/hyper([(-1,3),(1,3)],[1],1))
+def pi_015():
+    return simplify(3*sqrt(3)/2/hyper([S(-1)/3,S(1)/3],[1],1))
 
 # def pi_016():
 #     return simplify(8/(hyper([S(1)/2,S(1)/2],[1],S(1)/2)*gamma(S(3)/4)/gamma(S(5)/4))**2)
 
+# def pi_017():
+#     # SymPy doesn't have struvel
+#     return simplify(4*(hyper([1],[S(3)/2, 1],1)/struvel(-S(1)/2, 2))**2)
+
+def pi_018():
+    return simplify(1/meijerg([[],[]], [[0],[S(1)/2]], 0)**2)
+
+def pi_019():
+    # Note 1: simplify doesn't handle erfc -> erf
+    # Note 2: SymPy doesn't support the r argument to meijerg, so I removed it
+    # (doesn't matter because s is 1)
+    return simplify((meijerg([],[2], [1,S(3)/2],[], 1) / erfc(1))**2).rewrite(erf)
+
+def pi_020():
+    return simplify((1-E)/meijerg([[1],[S(1)/2]], [[1],[S(1)/2,0]], 1))
+
+def pi_021():
+    return simplify(sqrt(polygamma(1,S(1)/4)-8*Catalan))
+
+# SymPy doesn't have these functions yet
+
+# def pi_022():
+#     return simplify(elliprc(1,2)*4)
+#
+# def pi_023():
+#     return simplify(elliprg(0,1,1)*4)
+#
+# def pi_024():
+#     return simplify(2*agm(1,S(1)/2)*ellipk(S(3)/4))
+#
+# def pi_025():
+#     return (gamma(S(3)/4)*jtheta(3, 0, exp(-pi)))**4
+#
+# def pi_026():
+#     return root(gamma(S(1)/4)**4*agm(1, sqrt(2))**2/8, 3)
+
 def pi_027():
     return sqrt(6*zeta(2))
 
+def pi_028():
+    return sqrt(6*(zeta(2,3)+S(5)/4))
+
+# def pi_029():
+#     return simplify(sqrt(zeta(2,(3,4))+8*Catalan))
+
+# def pi_030():
+#     return simplify(exp(-2*zeta(x, 1).diff(x).subs(x, 0))/2)
+
+def pi_031():
+    return sqrt(12*dirichlet_eta(2))
+
+# SymPy doesn't have dirichlet yet
+
+# def pi_032():
+#     return simplify(4*dirichlet(1,[0,1,0,-1]))
+#
+# def pi_033():
+#     return simplify(2*Catalan/dirichlet(-1,[0,1,0,-1],1))
+#
+# def pi_034():
+#     return exp(-dirichlet(0,[0,1,0,-1],1))*gamma(S(1)/4)**2/(2*sqrt(2))
+
+# def pi_035():
+#     return simplify(sqrt(7*zeta(3)/(4*diff(lerchphi(-1,x,1), x)).subs(x, -2)))
+
+# SymPy is either wrong here, or there is some difference in definitions
+
+# def pi_036():
+#     return simplify(sqrt(-12*polylog(2,-1)))
+
+# def pi_037():
+#     simplify(sqrt(6*log(2)**2+12*polylog(2,S(1)/2)))
+
+# def pi_038():
+#     return simplify(root(-81*I*(polylog(3,root(1,3,1))+4*zeta(3)/9)/2,3))
+
+# SymPy doesn't have these functions
+
+# def pi_039():
+#     return 2*clsin(1,1)+1
+#
+# def pi_040():
+#     return (3+sqrt(3)*sqrt(1+8*clcos(2,1)))/2
+#
+# def pi_041():
+#     return root(2,6)*sqrt(e)/(glaisher**6*barnesg(S(1)/2)**4)
+
 def pi_042():
     return 4*summation((-1)**k/(2*k+1), (k,0,oo))
+
+# def pi_043():
+#     return summation((3**k-1)/4**k*zeta(k+1), (k, 1, oo))
+
+# Wrong result
+
+# def pi_044():
+#     return sqrt(summation(8/(2*k-1)**2, (k, 1, oo))).replace(exp_polar, exp)
 
 def pi_046():
     return summation(factorial(k)**2/factorial(2*k+1), (k,0,oo))*3*sqrt(3)/2
