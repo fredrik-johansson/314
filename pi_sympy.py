146183a6967c56e1c4159741f06a0aae1bd937cd
Gaurav Dhingra <axyd0000@gmail.com>
2016-03-11 22:42:50 +0530

    formulas for added SymPy "Pi's"

--- pi_sympy.py
+++ pi_sympy.py
@@ -70,13 +70,13 @@ def pi_107():
     return simplify(gamma(S(1)/4)*gamma(S(3)/4))/sqrt(2)
 
 def pi_108():
-    return simplify(integrate(1/(1 - x**3)**(S(1)/3), (x, 0, 1)))*9/(2*sqrt(3))
+    return simplify(integrate(3*sqrt(3)/(2*(1 - x**3)**(S(1)/3)), (x, 0, 1)))
 
 def pi_109():
     return (integrate(exp(-x**2)*cos(2*x), (x, 0, oo))*E*2)**2
 
 def pi_110():
-    return integrate(1/(1 + x**4), (x, -oo, oo))*sqrt(2)
+    return integrate(sqrt(2)/(1 + x**4), (x, -oo, oo))
 
 def pi_111():
     return integrate(x**2/(x**2 + S(1)/16)**2, (x, 0, oo))
